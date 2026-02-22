fa689e52bfe32e72e29c725a05d1cfdee9f87842
nichotienorsp <nicholas@berrijam.com>
2026-02-22 11:35:43 +0300

    fix: client error

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  turbopack: {},
-  reactStrictMode: true,
+  /* config options here */
 };
 
 export default nextConfig;
