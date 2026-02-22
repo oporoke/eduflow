8721269b68b25d84889eecae20fe2bb68f237a2b
nichotienorsp <nicholas@berrijam.com>
2026-02-22 11:26:52 +0300

    fix: client error

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack(config) {
+    // Make @ point to the project root
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": path.resolve(__dirname),
+    };
+    return config;
+  },
+  // optional: enable strict mode if not already
+  reactStrictMode: true,
 };
 
 export default nextConfig;
